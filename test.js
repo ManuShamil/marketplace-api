2928dc19238833890f247acef2e17bf5bfc67832
Manu Shamil <manusshamil@gmail.com>
2020-12-11 15:03:54 +0530

    edit lisitng

--- test.js
+++ test.js
@@ -13,7 +13,12 @@ const { buyItem } = require('./lib/market/buyItem')
 const { getMylistings } = require('./lib/accounts/getMylistings')
 const { buyCheapestItem } = require('./lib/market/buyCheapestItem')
 const { claimBalance } = require('./lib/accounts/claimBalance')
+const { addMarketItem } = require('./lib/market/addMarketItem')
+const { editItem  } = require('./lib/market/editItem')
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 connectToDatabase()
     .then( result => {
@@ -32,6 +37,20 @@ connectToDatabase()
             // }).catch(result => {
             //     console.log(result)
             // })
+
+            // for(var i =0; i < 1000; i++)
+            //     await addMarketItem("AK", getRandomInt(0, 5000), "76561198213882024", getRandomInt(0, 2))
+            //     .then( result => {
+            //         console.log(result)
+            //     }).catch(result => {
+            //         console.log(result)
+            //     })
+
+            await editItem({ _id: "5fd3248187a26d0b50969b76", listPrice : 1999}).then( result => {
+                console.log(result)
+            }).catch(result => {
+                console.log(result)
+            })
 
             // await refreshIndex().then( result => {
             //     console.log(result)
@@ -52,11 +71,11 @@ connectToDatabase()
             //     console.log(result)
             // })
 
-            await claimBalance("5fd2abfb8936ef32bcfff3ae").then( result => {
-                console.log(result)
-            }).catch(result => {
-                console.log(result)
-            })
+            // await claimBalance("5fd2abfb8936ef32bcfff3ae").then( result => {
+            //     console.log(result)
+            // }).catch(result => {
+            //     console.log(result)
+            // })
 
             
             // await addBalance("5fcf7b02608f6426908131db", 500).then( result => {
